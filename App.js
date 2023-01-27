1955c9ffbfb25864957dacd6c1971029dedd22c6
sfayadp <stefan0710f@gmail.com>
2023-01-26 23:19:31 -0500

    fix

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React from 'react';
-import { getChatGPTResponse } from './services/OpenAIService';
+import axios from 'axios';
+
+const API_KEY = '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';
+const API_ENDPOINT = 'https://api.openai.com/v1/engines/davinci/completions';
 
 
 export default function App() {
 
   const [text, setText] = React.useState("");
   const [textOutput, setTextOutput] = React.useState("");
-  
-  
+
+  const [loading, setLoading] = React.useState(false);
+
+  const getChatGPTResponse = async (prompt) => {
+    setLoading(true);
+    console.warn(prompt);
+    try {
+      const response = await axios.post(API_ENDPOINT, {
+        prompt: prompt,
+        max_tokens: 1000,
+      },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_KEY}`,
+          },
+        });
+      console.warn(response);
+      setTextOutput(response.data.choices[0].text);
+      setLoading(false);
+      return response.data.choices[0].text;
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
+
   const Search = async () => {
     setTextOutput('');
     const response = await getChatGPTResponse(text);
     console.warn(response);
-    setTextOutput(response);
   }
 
   const Clear = () => {
@@ -23,48 +50,49 @@ export default function App() {
   }
 
   return (
-    
+
     <View style={styles.container}>
-    <StatusBar style="auto" />
-    <Text style={styles.headerText}>OpenAI - React Native app</Text> 
-    <View style={styles.inputView}>
-      <TextInput
-        style={styles.TextInput}
-        placeholder="¿Escribe una pregunta?."
-        placeholderTextColor="#003f5c"
-        value={text}
-        onChangeText={setText}
-      /> 
-    </View> 
-    <View style={styles.textAreaView}>
-      <TextInput
-        style={styles.TextArea}
-        placeholderTextColor="#003f5c"
-        value={textOutput}
-        onChangeText={setTextOutput}
-        multiline = {true}
-        editable = {false}
-        numberOfLines = {4}
-      /> 
-    </View> 
-   
-   <View style={styles.buttonAreaView}>
-    <TouchableOpacity style={styles.searchBtn} onPress={ () => Search()} >
-        <Text style={styles.loginText}>Buscar</Text> 
-      </TouchableOpacity> 
-      <TouchableOpacity style={styles.cleanBtn} onPress={ () => Clear()} >
-        <Text style={styles.loginText}>Limpiar</Text> 
-      </TouchableOpacity> 
-   </View>
-   
-    
-  </View>
+      {loading && <ActivityIndicator size="large" color="#1e981e" />}
+      <StatusBar style="auto" />
+      <Text style={styles.headerText}>OpenAI - React Native app</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="¿Escribe una pregunta?."
+          placeholderTextColor="#003f5c"
+          value={text}
+          onChangeText={setText}
+        />
+      </View>
+      <View style={styles.textAreaView}>
+        <TextInput
+          style={styles.TextArea}
+          placeholderTextColor="#003f5c"
+          value={textOutput}
+          onChangeText={setTextOutput}
+          multiline={true}
+          editable={false}
+          numberOfLines={0}
+        />
+      </View>
+
+      <View style={styles.buttonAreaView}>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => Search()} >
+          <Text style={styles.loginText}>Buscar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.cleanBtn} onPress={() => Clear()} >
+          <Text style={styles.loginText}>Limpiar</Text>
+        </TouchableOpacity>
+      </View>
+
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
@@ -84,7 +112,7 @@ const styles = StyleSheet.create({
     height: 400,
     alignItems: "center",
   },
-  buttonAreaView:{
+  buttonAreaView: {
     flexDirection: 'row'
   },
   TextInput: {
@@ -92,7 +120,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     marginLeft: 20,
-  },  
+  },
   TextArea: {
     height: 100,
     flex: 1,
